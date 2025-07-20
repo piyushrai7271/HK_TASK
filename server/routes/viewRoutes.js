02f838fefcecd6d95a279d386666227946202dda
piyush rai <piyushrai7271@gmail.com>
2025-07-20 13:46:21 +0530

    get all task by user id

--- server/routes/viewRoutes.js
+++ server/routes/viewRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const Task = require('../models/task');
 
 // GET: Add User Form
 router.get('/add-user', (req, res) => {
@@ -16,6 +17,24 @@ router.get('/add-task', async (req, res) => {
     res.render('taskForm', { title: 'Add Task', users, success });
   } catch (err) {
     res.status(500).send("Error loading task form: " + err.message);
+  }
+});
+
+// View: Tasks by user ID
+router.get('/user/:id/tasks', async (req, res) => {
+  try {
+    const userId = parseInt(req.params.id);
+    const user = await User.query().findById(userId);
+    const tasks = await Task.query().where('user_id', userId);
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    res.render('userTasks', { user, tasks });
+  } catch (err) {
+    console.error('Error fetching tasks for user:', err);
+    res.status(500).send("Error loading tasks");
   }
 });
 
